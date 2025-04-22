1cc597e88608bf6998e98916ed85d2941a3c8892
Rahul-atmecs <rahul.chatterjee@atmecs.com>
2025-04-22 14:41:21 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import datetime
 
 def main():
     now = datetime.datetime.now()
-    print(f"[{now}] Hello from Jenkins via GitHub Poll SCM! 2:36 commit")
+    print(f"[{now}] Hello from Jenkins via GitHub Poll SCM! 2:41 commit")
 
 if __name__ == "__main__":
     main()
